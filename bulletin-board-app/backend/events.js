787ae8046fefe671e21b671883b82a7b573af669
Tashi-p <45115415+Tashi-p@users.noreply.github.com>
2019-11-22 16:16:01 +0530

    My commit
    This includes all the files and folder

---
+++ bulletin-board-app/backend/events.js
@@ -0,0 +1,19 @@
+module.exports = [
+    {
+      id: 1,
+      title: 'Docker Workshop',
+      detail: 'MSIT-IIIT-H',
+      date: '2019-11-21'
+    },
+    {
+      id: 2,
+      title: 'DevOpps ',
+      detail: 'Introductory for DevOpps',
+      date: '2019-11-14'
+    },
+    {
+      id: 3,
+      title: 'Jenkins',
+      date: '2019-10-13'
+    }
+  ];
